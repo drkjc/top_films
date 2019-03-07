29a573d7af2948a722777c7da0b86cffd26c7a83
github username <github email address>
2019-03-06 17:55:29 -0700

    second page scrape added for full 100 films

--- lib/top_films/scraper.rb
+++ lib/top_films/scraper.rb
@@ -2,16 +2,36 @@ require_relative './version'
 
 class TopFilms::Scraper
 
-  def get_page
+  def get_first_50
     #title = nil, rating = nil, year = nil, description = nil, genre, rating
-    page = Nokogiri::HTML(open("https://www.imdb.com/search/title?groups=top_250&sort=user_rating"))
-    title = page.css("h3.lister-item-header a").first.text
-    rating = page.css("div.ratings-imdb-rating strong").first.text
-    year = page.css("h3.lister-item-header span.lister-item-year").first.text
-    description = page.css("div.lister-item-content p.text-muted")[1].text.strip
-    genre = page.css("span.genre").first.text.split.join
-    rating = page.css("p.text-muted span.certificate").first.text
+    page1 = Nokogiri::HTML(open("https://www.imdb.com/search/title?groups=top_250&sort=user_rating"))
+    # @title = page1.css("h3.lister-item-header a").first.text
+    # @rating = page1.css("div.ratings-imdb-rating strong").first.text
+    # @year = page1.css("h3.lister-item-header span.lister-item-year").first.text
+    # @description = page1.css("div.lister-item-content p.text-muted")[1].text.strip
+    # @genre = page1.css("span.genre").first.text.split.join
+    # @advisory = page1.css("p.text-muted span.certificate").first.text
+  end
+
+  def get_second_50
+    page2 = Nokogiri::HTML(open("https://www.imdb.com/search/title?groups=top_250&sort=user_rating,desc&start=51&ref_=adv_nxt"))
+    title = page2.css("h3.lister-item-header a").first.text
+    rating = page2.css("div.ratings-imdb-rating strong").first.text
+    year = page2.css("h3.lister-item-header span.lister-item-year").first.text
+    description = page2.css("div.lister-item-content p.text-muted")[1].text.strip
+    genre = page2.css("span.genre").first.text.split.join
+    advisory = page2.css("p.text-muted span.certificate").first.text
     binding.pry
+  end
+
+  def scrape_film_index
+    index1 = page1.css("span.text-primary").text.split(".").collect { |i| i.to_i}
+    index2 = page2.css("span.text-primary").text.split(".").collect { |i| i.to_i}
+    self.get_page
+  end
+
+  def make_films
+    TopFilms::Film.new(get_page)
   end
 
 end
