778cb16245d5e7b52c8757691ad3018f91d89755
github username <github email address>
2019-03-14 10:49:57 -0700

    if/else changed to case in CLI/added aesethetics to guide user

--- lib/top_films/cli.rb
+++ lib/top_films/cli.rb
@@ -2,7 +2,8 @@ require_relative './version'
 require_relative './film'
 require_relative './scraper'
 
-puts "Welcome! Please hold while I gather your film list."
+puts
+puts "Welcome to TopFilms! Please hold while I gather your film list."
 puts
 
 class TopFilms::CLI
@@ -11,36 +12,43 @@ class TopFilms::CLI
 
   TopFilms::Scraper.new.make_films
 
+  puts "These are the top 100 films of all time.".light_yellow
+  sleep(2)
+  puts "The films have been divided into five lists.".light_magenta
+  sleep(2)
+  puts "They are ranked according to IMDB user rating.".light_yellow
+  sleep(3)
+
   def call
-    puts "These are the top 100 films of all time based on user rating."
     puts
     start
     interact
   end
 
   def start
+    lineWidth = 80
+    puts ("Enter a letter to see the corresponding list.".center(lineWidth)).colorize(:color => :red, :background => :light_white)
     puts <<~DOC
-    Films are in ranked order. 
-    Type a letter to see the corresponding list.
 
-    A (1-25)
-    B (26-50)
-    C (51-75)
-    D (76-100)
-    X Surprise me!
-
+    'A' for films (1-20)
+    'B' for films (21-40)
+    'C' for films (41-60)
+    'D' for films (61-80)
+    'E' for films (81-100)
+    'X' for a surprise film!
     DOC
     @list_letter = gets.strip.downcase
   end
 
   def interact
-    puts
+    lineWidth = 40
 
+    puts
     film_list(list_letter)
-    puts
 
-    puts "Type rank number to explore the film, or 'exit'."
-    puts
+    puts ("Enter rank number to explore the film.".center(lineWidth)).colorize(:color => :red, :background => :light_white)
+    puts ("or".center(lineWidth)).light_yellow
+    puts ("'E' to exit.".center(lineWidth)).colorize(:color => :red, :background => :light_white)
 
     @rank_num = gets.strip.downcase
 
@@ -54,23 +62,28 @@ class TopFilms::CLI
   end
 
   def film_list(list_letter)
-    if list_letter == "a"
-      TopFilms::Film.all[0..24].each.with_index(1) do |film, i|
-        puts "#{i}. #{film.title}"
+    case list_letter
+    when 'a'
+      TopFilms::Film.all[0..19].each.with_index(1) do |film, i|
+        puts "#{i} #{film.title}"
       end
-    elsif list_letter == "b"
-      TopFilms::Film.all[25..49].each.with_index(26) do |film, i|
-        puts "#{i}. #{film.title}"
+    when 'b'
+      TopFilms::Film.all[20..39].each.with_index(21) do |film, i|
+        puts "#{i} #{film.title}"
       end
-    elsif list_letter == "c"
-      TopFilms::Film.all[50..74].each.with_index(51) do |film, i|
-        puts "#{i}. #{film.title}"
+    when 'c'
+      TopFilms::Film.all[40..59].each.with_index(41) do |film, i|
+        puts "#{i} #{film.title}"
       end
-    elsif list_letter == "d"
-      TopFilms::Film.all[75..99].each.with_index(76) do |film, i|
-        puts "#{i}. #{film.title}"
+    when 'd'
+      TopFilms::Film.all[60..79].each.with_index(61) do |film, i|
+        puts "#{i} #{film.title}"
       end
-    elsif list_letter == "x"
+    when 'e'
+      TopFilms::Film.all[80..99].each.with_index(81) do |film, i|
+        puts "#{i} #{film.title}"
+      end
+    when 'x'
       film = TopFilms::Film.all.sample
       @rank_num = TopFilms::Film.all.find_index(film) + 1
       film_description(film)
@@ -88,6 +101,7 @@ class TopFilms::CLI
 
   def film_description(film)
     lineWidth = 80
+    puts
     puts ("~*~*~*~*~  #{rank_num}. #{film.title} #{film.year}  ~*~*~*~*~".center(lineWidth)).light_yellow
     puts
     puts ("#{film.advisory} | #{film.run_time} | #{film.genre}".center(lineWidth))
@@ -95,20 +109,18 @@ class TopFilms::CLI
     puts ("#{film.rating}/10 based on #{film.votes} votes.".center(lineWidth))
     puts
     puts ("____________Description:____________".center(lineWidth)).light_yellow
-    puts
     puts ("#{film.description}".center(lineWidth))
     puts ("------------------------------------".center(lineWidth)).light_yellow
     puts ("Director: #{film.director}".center(lineWidth))
     puts
     puts ("Stars: #{film.lead_actors}".center(lineWidth))
-    puts
     puts ("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*".center(lineWidth)).light_yellow
     puts
   end
 
   def menu_bar
     lineWidth = 40
-    puts ("enter 'L' to see the list again".ljust(lineWidth)).colorize(:color => :red, :background => :light_white)
+    puts ("enter 'L' to see the list/random film".ljust(lineWidth)).colorize(:color => :red, :background => :light_white)
     puts
     puts ("enter 'R' to see the rankings".ljust(lineWidth)).colorize(:color => :red, :background => :light_white)
     puts
@@ -122,15 +134,17 @@ class TopFilms::CLI
       call
     elsif input == "e"
       puts "See you next time!".light_yellow
+      puts
       exit
     else
       puts "Please try again.".red
-      call
+      puts
+      menu_bar
     end
   end
 
   def num_works?(rank_num)
-    if @rank_num == 'exit'
+    if @rank_num == 'e'
       exit
     elsif (1..100).to_a.include?(rank_num.to_i) == false
       @rank_num = " "
